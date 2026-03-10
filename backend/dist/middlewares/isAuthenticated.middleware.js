d4a37a4a16c522305fe199b82e8ea6691cb746b8
ggpavithra123 <158811352+ggpavithra123@users.noreply.github.com>
2026-03-10 12:50:58 +0530

    Add logging in isAuthenticated middleware
    Added logging for session and user information in the authentication middleware.

--- backend/dist/middlewares/isAuthenticated.middleware.js
+++ backend/dist/middlewares/isAuthenticated.middleware.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const appError_1 = require("../utils/appError");
 const isAuthenticated = (req, res, next) => {
+    console.log("SESSION:", req.session);
+    console.log("USER:", req.user);
     if (!req.user || !req.user._id) {
         throw new appError_1.UnauthorizedException("Unauthorized. Please log in.");
     }
