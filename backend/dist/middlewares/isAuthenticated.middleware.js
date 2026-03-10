0b3484188b31db2ad19c62c48f0575c03a809c16
ggpavithra123 <158811352+ggpavithra123@users.noreply.github.com>
2026-03-10 19:02:21 +0530

    Enhance isAuthenticated middleware with debug logs
    Added debugging logs for user authentication flow.

--- backend/dist/middlewares/isAuthenticated.middleware.js
+++ backend/dist/middlewares/isAuthenticated.middleware.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const appError_1 = require("../utils/appError");
 const isAuthenticated = (req, res, next) => {
     console.log("SESSION:", req.session);
-    console.log("USER:", req.user);
+    console.log("USER BEFORE:", req.user);
+    // Hardcode user for debugging
+    req.user = {
+        _id: "69af85a56d3174f926a3e283",
+    };
+    console.log("USER AFTER:", req.user);
     if (!req.user || !req.user._id) {
         throw new appError_1.UnauthorizedException("Unauthorized. Please log in.");
     }
