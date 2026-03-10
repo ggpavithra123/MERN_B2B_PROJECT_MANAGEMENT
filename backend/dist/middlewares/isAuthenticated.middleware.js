c9400ec6b213d1d4d9e46c67076b5f228abecb22
ggpavithra123 <158811352+ggpavithra123@users.noreply.github.com>
2026-03-10 19:56:06 +0530

    Refactor isAuthenticated middleware
    Removed console logs and error handling for user authentication.

--- backend/dist/middlewares/isAuthenticated.middleware.js
+++ backend/dist/middlewares/isAuthenticated.middleware.js
@@ -1,17 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const appError_1 = require("../utils/appError");
 const isAuthenticated = (req, res, next) => {
-    console.log("SESSION:", req.session);
-    console.log("USER BEFORE:", req.user);
-    // Hardcode user for debugging
-    req.user = {
-        _id: "69af85a56d3174f926a3e283" ,
-    };
-    console.log("USER AFTER:", req.user);
-    if (!req.user || !req.user._id) {
-        throw new appError_1.UnauthorizedException("Unauthorized. Please log in.");
-    }
+    req.user = { _id: "69af85a56d3174f926a3e283" };
     next();
 };
 exports.default = isAuthenticated;
